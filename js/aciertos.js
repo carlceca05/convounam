7163452205d639d23417e0d77e0784cdad61a371
carlceca05 <86792334+carlceca05@users.noreply.github.com>
2022-01-07 13:04:46 -0600

    se agrego el archivo

---
+++ js/aciertos.js
@@ -0,0 +1,79 @@
+function MostrarSeleccionado(){
+    var cod = document.getElementById("carrerasunam").value;
+    if(cod == 1){
+        document.getElementById("actuaria").classList.toggle("show");
+    }
+    if(cod == 2 ){
+     document.getElementById("administracion").classList.toggle("show");
+    }
+    if(cod == 3 ){
+     document.getElementById("administracion-agropecuaria").classList.toggle("show");
+    }
+    if(cod == 4 ){
+     document.getElementById("administraciondearchivosygestiondocumental").classList.toggle("show");
+    }
+    if(cod == 5 ){
+     document.getElementById("antropologia").classList.toggle("show");
+    }
+    if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+    if(cod == 7 ){
+        document.getElementById("arquitecturadelpaisaje").classList.toggle("show");
+       }
+    if(cod == 8 ){
+        document.getElementById("arteydiseño").classList.toggle("show");
+       }
+       if(cod == 9 ){
+        document.getElementById("artesvisuales").classList.toggle("show");
+       }
+       if(cod == 10 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+       if(cod == 6 ){
+        document.getElementById("arquitectura").classList.toggle("show");
+       }
+    
+}
